a63c1f561fa03428f94e66e7c1d4f574d34e3395
Lukman <lukmanmakhaev@gmail.com>
2024-08-17 18:53:28 +0300

    Initial Commit

---
+++ SparInternTask/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SparInternTask
+//
+//  Created by Lukman Makhaev on 17.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SparInternTask/SparInternTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparInternTaskApp.swift
+//  SparInternTask
+//
+//  Created by Lukman Makhaev on 17.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparInternTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
